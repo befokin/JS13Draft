f1b08559d3d42bf639897c3bb711c7ca63660995
Boris Fokin <befokin@mail.ru>
2020-06-19 13:55:21 +0300

    draft_05

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-fsgsg
+const doNum = function(a, b, callback) {
+      if(typeof a === 'number' && typeof b === 'number' ) {
+            callback(a, b);
+      }
+}
+
+doNum(5, 10, function(a, b) {
+      console.log(a + b);
+});
+
+function mult (a, b) {
+      console.log(a * b);
+}
+
+doNum(10, 5, mult);
